fcda6d2ad2c1634d1d53f1792413792f68557221
shaman-004 <shamanmujukoomule2004@gmail.com>
2022-10-24 00:05:02 +0530

    fibonacci

---
+++ Type#2/UploadHere/shaman-004/fibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fibonacci(int n);
+void main()
+{
+int n,i=0,j;
+scanf("%d",&n);
+printf("fibonacci series");
+for(j=1;j<=n;j++)
+{
+    printf("%d/n",fibonacci(i));
+    i++;
+
+}
+}
+int fibonacci(int n)
+{
+    if(n==0)
+    return 0;
+    else if(n==1)
+    return 1;
+    else
+    return(fibonacci(n-1)+fibonacci(n-2));
+}
