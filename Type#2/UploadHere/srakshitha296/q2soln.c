c56f081ef29605dff4b63f7b8da85293e6a2cee8
srakshitha296 <srakshitha296@gmail.com>
2022-10-25 00:58:23 +0530

    added a file

---
+++ Type#2/UploadHere/srakshitha296/q2soln.c
@@ -0,0 +1,27 @@
+/*Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int n,i,j;
+    printf("Enter a string : ");
+    scanf("%s",str);
+    n=strlen(str);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(str[i]==str[j]&&str[i]!='"')
+            {
+            printf("false");
+            exit(0);
+            }
+        }
+    }
+    printf("true");
+    return 0;
+}
